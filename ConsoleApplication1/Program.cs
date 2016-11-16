30ed5a34ac2b0fe68a3e8adae7fde8fc575924c5
Dianne Cree <Dianne_Cree@shi.com>
2016-11-15 18:30:10 -0600

    ammending last check out

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,6 +10,10 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            //
+            var x = 0;
+            args[0] = x.ToString();
+
         }
     }
 }
